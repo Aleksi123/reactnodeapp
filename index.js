8041266f58c701b44ea74b6950d644cc83ca2209
Aleksi <a12345@live.fi>
2021-02-22 15:50:41 +0200

    Db connection using Sequelize
    - Connected to MySQL db using Sequelize
    - Installed mysql2, sequelize and concurrently packages
    - Changed backend port from 3000 to 4000

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express')
+const { Sequelize } = require('sequelize');
+
 const app = express()
-const port = 3000
+const port = 4000
+
+// Connect to MySQL DB using Sequelize ORM
+const sequelize = new Sequelize('db', 'user', 'password', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+sequelize
+  .authenticate()
+  .then(() => console.log('Connected to db.'))
+  .catch(error => console.log('Unable to connect to the database:', error))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
